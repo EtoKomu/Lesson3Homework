a3708c801df26133c7068ca4ea80bed52d06a548
EtoKomu <peepoetokomu@gmail.com>
2023-06-03 22:45:14 +0500

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.Write("Таблица кубов от 1 до введенного числа: ");
+int index = 1;
+while (index <= num)
+{
+    Console.Write(index * index * index + ", ");
+    index += 1;
+}
